f6002eee68c9b0aa4531f4f9721baa7b9486b9c2
timonvlad <timoshkinvlad@gmail.com>
2013-03-05 06:57:15 +0300

    5.03.13 10:55

--- src/tsysv/anti/ttt/MainActivity.java
+++ src/tsysv/anti/ttt/MainActivity.java
@@ -2,14 +2,100 @@ package tsysv.anti.ttt;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
 
+	Button mainnext;
+	CheckBox check_sms, check_mail;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Boolean checked_sms = getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedsms", false);
+		Boolean checked_mail = getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedmail", false);
+		
+		check_mail = (CheckBox) findViewById(R.id.cbsendmail);
+		check_sms = (CheckBox) findViewById(R.id.cbsendsms);
+		
+		if (!checked_sms) {
+			check_sms.setChecked(check_sms.isChecked());
+		} else {
+			check_sms.setChecked(!check_sms.isChecked());
+		}// ///////////////////////
+		if (!checked_mail) {
+			check_mail.setChecked(check_mail.isChecked());
+		} else {
+			check_mail.setChecked(!check_mail.isChecked());
+		}// ///////////////////////
+		
+		
+		///////////////////
+			check_sms.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					getSharedPreferences("antithief", MODE_PRIVATE).edit()
+					.putBoolean("checkedsms", true)
+					.commit();
+					Log.v("CHECKED SMS", "is - " + getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedsms", false));
+				}
+				else{
+					getSharedPreferences("antithief", MODE_PRIVATE).edit()
+					.putBoolean("checkedsms", false)
+					.commit();
+					Log.v("CHECKED SMS", "is - " + getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedsms", false));
+				}
+				
+			}
+		});
+		///////////////////
+			check_mail.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					getSharedPreferences("antithief", MODE_PRIVATE).edit()
+					.putBoolean("checkedmail", true)
+					.commit();
+					Log.v("CHECKED mail", "is - " + getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedmail", false));
+				}
+				else{
+					getSharedPreferences("antithief", MODE_PRIVATE).edit()
+					.putBoolean("checkedmail", false)
+					.commit();
+					Log.v("CHECKED mail", "is - " + getSharedPreferences("antithief", MODE_PRIVATE)
+				.getBoolean("checkedmail", false));
+				}
+				
+			}
+		});
+		
+		
+		mainnext = (Button) findViewById(R.id.mainnext);
+		mainnext.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				
+				
+			}
+		});
 	}
 
 	@Override
