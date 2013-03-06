f69988b095432f24665f2f113e943b7ee06cbd69
timonvlad <timoshkinvlad@gmail.com>
2013-03-06 08:25:50 +0300

    6/3 begin

--- src/tsysv/anti/ttt/Constants.java
+++ src/tsysv/anti/ttt/Constants.java
@@ -2,5 +2,6 @@ package tsysv.anti.ttt;
 
 public class Constants {
 	public static final String em = "**********@gmail.com";
+	///////////////////
 	public static final String ps = "**************";
 }
